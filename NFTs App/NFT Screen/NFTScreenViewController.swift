a5bd9f5442b29fc0ebd5e01d7d33c3cfaa1b684f
Alexandros Pallikaridis <75374244+Alexpal00@users.noreply.github.com>
2023-06-28 13:04:41 +0300

    Add files via upload

--- NFTs App/NFT Screen/NFTScreenViewController.swift
+++ NFTs App/NFT Screen/NFTScreenViewController.swift
@@ -15,7 +15,11 @@ enum TableViewDataEnum {
 
 class NFTScreenViewController: UIViewController {
     
+    // MARK: - IBPProperties
+    
     @IBOutlet weak var tableView: UITableView?
+    
+    // MARK: - Properties
     
     var nfts = [NftsResponse]()
     var titleNFTS = [NftsResponse]()
@@ -23,33 +27,40 @@ class NFTScreenViewController: UIViewController {
     var emptyView: EmptyView?
     
     var data: [TableViewDataEnum] = []
+    
+    // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        tableView?.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomTableViewCell")
+        tableView?.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomTableViewCellIdentifier")
         tableView?.register(UINib(nibName: "CollectionTableViewCell", bundle: nil), forCellReuseIdentifier: "CollectionTableViewCellIdentifier")
         
         //Styling the table
         tableView?.separatorStyle = .singleLine
         tableView?.separatorColor = .black
         tableView?.showsVerticalScrollIndicator = false
+       
         
         //Set data source and delegate
         tableView?.delegate = self
         tableView?.dataSource = self
-       
+       /*
         downloadJSON {
             self.tableView?.reloadData()
             print("success")
         }
+        */
+        
+        readJSONFile()
     }
     
-   
+    // MARK: - Methods
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
     //prepare me to enum
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -60,7 +71,9 @@ class NFTScreenViewController: UIViewController {
                 }
                 }
             }
-  
+    
+    //Parsing JSON via URL
+  /*
     func downloadJSON(completed: @escaping () -> ()) {
         DispatchQueue.global(qos: .userInitiated).async {
             guard let url = URL(string: "https://public.arx.net/~chris2/nfts1.json") else {
@@ -99,7 +112,57 @@ class NFTScreenViewController: UIViewController {
             }
         }
     }
+    */
     
+    //Parsing JSON from local file
+    
+    func readJSONFile() {
+        guard let path = Bundle.main.path(forResource: "MyJSON", ofType: "json") else {
+            print ("JSON file could not be found")
+            return
+        }
+        
+        do {
+                let jsonData = try Data(contentsOf: URL(fileURLWithPath: path))
+                let nftsResponse = try JSONDecoder().decode(NftsResponse.self, from: jsonData)
+                
+                //self.nfts = [nftsResponse] //Isos to sviso
+                self.data.removeAll()
+                
+                self.nfts.append(nftsResponse) // Emfanizei ola ta customCell apo to JSON me ta enum (5)
+                
+                self.data.append(.banner(nfts: [nftsResponse]))
+                
+                for nft in nftsResponse.nfts {
+                    self.data.append(.element(nft: nft))
+                }
+                
+                tableView?.reloadData()
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }
+    }
+    /*
+    func updateTableViewData() {
+            data.removeAll()
+            
+            // Add the banner data
+            if !nfts.isEmpty {
+                data.append(.banner(nfts: nfts))
+            }
+            
+            // Add the element data
+            for nft in titleNFTS {
+                data.append(.element(nft: nft))
+            }
+            
+            // Reload the table view
+            tableView?.reloadData()
+        }
+    */
+    
+    /*
     func showEmptyView(with message: String) {
         emptyView = EmptyView(delegate: self, message: message)
         view.addSubview(emptyView!)
@@ -109,4 +172,5 @@ class NFTScreenViewController: UIViewController {
         emptyView?.removeFromSuperview()
         emptyView = nil
     }
-}
+    */
+
